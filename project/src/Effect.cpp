d3c4cc8c37248cf781d5787a657e2f2f627730d6
Luna <luna.pype06@gmail.com>
2025-12-26 19:37:09 +0100

    Fixed

--- project/src/Effect.cpp
+++ project/src/Effect.cpp
@@ -20,8 +20,8 @@ ID3DX11Effect* Effect::LoadEffect( ID3D11Device* pDevice, const std::wstring& as
 
 	DWORD shaderFlags{};
 #if defined( DEBUG ) || defined( _DEBUG )
-	shaderflags |= D3DCOMPILE_DEBUG;
-	shaderflags |= D3DCOMPILE_SKIP_OPTIMIZATION;
+	shaderFlags |= D3DCOMPILE_DEBUG;
+	shaderFlags |= D3DCOMPILE_SKIP_OPTIMIZATION;
 #endif
 	result = D3DX11CompileEffectFromFile(
 		assetFile.c_str(), nullptr, nullptr, shaderFlags, 0, pDevice, &pEffect, &pErrorBlob );
